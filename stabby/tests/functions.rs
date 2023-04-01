41bc7921eb63479e661c382c27347aaec89d3441
Pierre Avital <pierre.avital@me.com>
2023-04-01 16:16:45 +0200

    remove always panicking test that was used for debugging

--- stabby/tests/functions.rs
+++ stabby/tests/functions.rs
@@ -1,9 +1,2 @@
 #[stabby::export]
 extern "C" fn stable_fn(_: u8) {}
-
-#[test]
-fn report() {
-    use stabby::IStable;
-    let report = <extern "C" fn(u8) as IStable>::REPORT;
-    panic!("{:?}", report);
-}
