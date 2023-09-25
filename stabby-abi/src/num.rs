2253b8a64adee68ea940495654d7a0e7e6981b8e
Pierre Avital <pierre.avital@me.com>
2023-09-25 17:28:14 +0200

    improve test coverage

--- stabby-abi/src/num.rs
+++ stabby-abi/src/num.rs
@@ -142,13 +142,6 @@ define_non_max!(NonMaxU64: u64 = core::num::NonZeroU64);
 define_non_max!(NonMaxU128: u128 = core::num::NonZeroU128);
 define_non_max!(NonMaxUsize: usize = core::num::NonZeroUsize);
 
-define_non_max!(NonMinI8: i8 = core::num::NonZeroI8);
-define_non_max!(NonMinI16: i16 = core::num::NonZeroI16);
-define_non_max!(NonMinI32: i32 = core::num::NonZeroI32);
-define_non_max!(NonMinI64: i64 = core::num::NonZeroI64);
-define_non_max!(NonMinI128: i128 = core::num::NonZeroI128);
-define_non_max!(NonMinIsize: isize = core::num::NonZeroIsize);
-
 define_non_x!(NonXU8: u8 = core::num::NonZeroU8);
 define_non_x!(NonXU16: u16 = core::num::NonZeroU16);
 define_non_x!(NonXU32: u32 = core::num::NonZeroU32);
@@ -162,18 +155,51 @@ define_non_x!(NonXI64: i64 = core::num::NonZeroI64);
 define_non_x!(NonXI128: i128 = core::num::NonZeroI128);
 define_non_x!(NonXIsize: isize = core::num::NonZeroIsize);
 
-#[test]
-fn test() {
-    for i in 0..255 {
-        assert_eq!(NonMaxU8::new(i).unwrap().get(), i);
-        assert_eq!(NonXU8::<255>::new(i).unwrap().get(), i);
-    }
-    assert!(NonMaxU8::new(255).is_none());
-    assert!(NonXU8::<255>::new(255).is_none());
-    assert!(NonXU8::<72>::new(72).is_none());
-    for i in 0..=255 {
-        if i != 72 {
-            assert_eq!(NonXU8::<72>::new(i).unwrap().get(), i);
+macro_rules! makeutest {
+    ($u8: ident, $NonMaxU8: ident, $NonXU8: ident) => {
+        #[test]
+        fn $u8() {
+            for i in 0..255 {
+                assert_eq!($NonMaxU8::new(i).unwrap().get(), i);
+                assert_eq!($NonXU8::<{ $u8::MAX }>::new(i).unwrap().get(), i);
+            }
+            assert!($NonMaxU8::new($u8::MAX).is_none());
+            assert!($NonXU8::<{ $u8::MAX }>::new($u8::MAX).is_none());
+            assert!($NonXU8::<72>::new(72).is_none());
+            for i in 0..=255 {
+                if i != 72 {
+                    assert_eq!($NonXU8::<72>::new(i).unwrap().get(), i);
+                }
+            }
         }
-    }
+    };
 }
+makeutest!(u8, NonMaxU8, NonXU8);
+makeutest!(u16, NonMaxU16, NonXU16);
+makeutest!(u32, NonMaxU32, NonXU32);
+makeutest!(u64, NonMaxU64, NonXU64);
+makeutest!(u128, NonMaxU128, NonXU128);
+makeutest!(usize, NonMaxUsize, NonXUsize);
+macro_rules! makeitest {
+    ($i8: ident,  $NonXI8: ident) => {
+        #[test]
+        fn $i8() {
+            for i in -127..=127 {
+                assert_eq!($NonXI8::<{ $i8::MIN }>::new(i).unwrap().get(), i);
+            }
+            assert!($NonXI8::<{ $i8::MIN }>::new($i8::MIN).is_none());
+            assert!($NonXI8::<72>::new(72).is_none());
+            for i in -128..=127 {
+                if i != 72 {
+                    assert_eq!($NonXI8::<72>::new(i).unwrap().get(), i);
+                }
+            }
+        }
+    };
+}
+makeitest!(i8, NonXI8);
+makeitest!(i16, NonXI16);
+makeitest!(i32, NonXI32);
+makeitest!(i64, NonXI64);
+makeitest!(i128, NonXI128);
+makeitest!(isize, NonXIsize);
