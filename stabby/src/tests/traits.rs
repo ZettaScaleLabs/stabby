0f07a589d147b1c41ee96e706b368ed73d450ef6
matejcik <ja@matejcik.cz>
2026-02-18 14:09:50 +0100

    feat: gate "alloc" requirement behind a feature
    fixes usage in bare metal no-std environments
    fixes #93

--- stabby/src/tests/traits.rs
+++ stabby/src/tests/traits.rs
@@ -17,6 +17,7 @@
 #![cfg_attr(stabby_unsafe_wakers = "true", allow(deprecated))]
 
 pub use crate as stabby;
+#[cfg(feature = "alloc")]
 use stabby::boxed::Box;
 
 #[stabby::stabby(checked)]
@@ -76,6 +77,7 @@ pub trait MyTrait3<Hi: core::ops::Deref> {
     extern "C" fn test2(&mut self);
 }
 
+#[cfg(feature = "alloc")]
 impl MyTrait3<Box<()>> for u8 {
     type A = u8;
     type B = u8;
@@ -88,6 +90,8 @@ impl MyTrait3<Box<()>> for u8 {
     extern "C" fn test(&mut self) {}
     extern "C" fn test2(&mut self) {}
 }
+
+#[cfg(feature = "alloc")]
 impl MyTrait3<Box<()>> for u16 {
     type A = u8;
     type B = u8;
@@ -101,6 +105,7 @@ impl MyTrait3<Box<()>> for u16 {
     extern "C" fn test2(&mut self) {}
 }
 
+#[cfg(feature = "alloc")]
 #[stabby::stabby(checked)]
 pub trait AsyncRead {
     extern "C" fn read<'a>(
@@ -108,6 +113,7 @@ pub trait AsyncRead {
         buffer: stabby::slice::SliceMut<'a, u8>,
     ) -> stabby::future::DynFuture<'a, usize>;
 }
+#[cfg(feature = "alloc")]
 impl AsyncRead for stabby::slice::Slice<'_, u8> {
     extern "C" fn read<'a>(
         &'a mut self,
@@ -125,6 +131,7 @@ impl AsyncRead for stabby::slice::Slice<'_, u8> {
     }
 }
 
+#[cfg(feature = "alloc")]
 #[test]
 fn dyn_traits() {
     let boxed = Box::new(6u8);
